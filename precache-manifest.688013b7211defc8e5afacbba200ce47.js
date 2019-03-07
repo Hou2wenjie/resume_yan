21556614451bcef1ac8d59a63415637d4c8b030f
wenjiehou <hou2wenjie@outlook.com>
2019-03-07 22:16:53 +0100

    Updates

---
+++ precache-manifest.688013b7211defc8e5afacbba200ce47.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5cf54948bea90539d51",
+    "url": "/resume_yan/static/css/main.e2a69258.chunk.css"
+  },
+  {
+    "revision": "b5cf54948bea90539d51",
+    "url": "/resume_yan/static/js/main.b5cf5494.chunk.js"
+  },
+  {
+    "revision": "4e6410adeea702242621",
+    "url": "/resume_yan/static/js/runtime~main.4e6410ad.js"
+  },
+  {
+    "revision": "0551f4249cd443894a99",
+    "url": "/resume_yan/static/js/2.0551f424.chunk.js"
+  },
+  {
+    "revision": "0125aca4a46a1faac06529ffb4cb0fee",
+    "url": "/resume_yan/static/media/xy.0125aca4.jpeg"
+  },
+  {
+    "revision": "099ec8c409187fb5adee73e953e3c675",
+    "url": "/resume_yan/static/media/UNI_Bonn.099ec8c4.jpg"
+  },
+  {
+    "revision": "05691cf4df77cddb93c3848ed7afbbf2",
+    "url": "/resume_yan/static/media/wufe.05691cf4.png"
+  },
+  {
+    "revision": "24307d80088edeee0097f71e85dece30",
+    "url": "/resume_yan/static/media/abc.24307d80.jpg"
+  },
+  {
+    "revision": "1ec9e885e32ff7407729128583b1e7d8",
+    "url": "/resume_yan/static/media/BeiDou.1ec9e885.ico"
+  },
+  {
+    "revision": "b41de31ed18230dd1c5ed66acaefe896",
+    "url": "/resume_yan/static/media/cfa-opengraph-share-image.b41de31e.png"
+  },
+  {
+    "revision": "cdb0ebfe1438a72b3e88c4ded1dc13c9",
+    "url": "/resume_yan/static/media/unnamed.cdb0ebfe.jpg"
+  },
+  {
+    "revision": "89e2133c1b043a449149854e11ea9354",
+    "url": "/resume_yan/index.html"
+  }
+];
